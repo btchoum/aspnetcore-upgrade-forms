bb6bc3998ccf091ab9a75903bd4370ce03d9a18c
btchoum <bertrand.tchoumkeu@gmail.com>
2022-05-14 08:26:33 -0500

    add tickets in memory repository

---
+++ SimpleHelpDesk.Data/DataAccess/ITicketsRepository.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using SimpleHelpDesk.Data.Entities;
+
+namespace SimpleHelpDesk.Data.DataAccess
+{
+    public interface ITicketsRepository
+    {
+        IEnumerable<Ticket> GetAll();
+        Ticket GetById(long id);
+        long Add(Ticket ticket);
+        void Update(Ticket ticket);
+        void Remove(long id);
+    }
+}
---
+++ SimpleHelpDesk.Data/DataAccess/TicketInMemoryRepository.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using SimpleHelpDesk.Data.Entities;
+
+namespace SimpleHelpDesk.Data.DataAccess
+{
+    public class InMemoryTicketsRepository : ITicketsRepository
+    {
+        private static readonly Dictionary<long, Ticket>
+            Tickets = new Dictionary<long, Ticket>();
+
+        public IEnumerable<Ticket> GetAll()
+        {
+            return Tickets.Values.ToList();
+        }
+
+        public Ticket GetById(long id)
+        {
+            return Tickets.ContainsKey(id) ? Tickets[id] : null;
+        }
+
+        public long Add(Ticket ticket)
+        {
+            ticket.Id = Tickets.Keys.Count == 0 ? 1 : Tickets.Keys.Max() + 1;
+
+            Tickets[ticket.Id] = ticket;
+
+            return ticket.Id;
+        }
+
+        public void Update(Ticket ticket)
+        {
+            Tickets[ticket.Id] = ticket;
+        }
+
+        public void Remove(long id)
+        {
+            Tickets.Remove(id);
+        }
+    }
+}
